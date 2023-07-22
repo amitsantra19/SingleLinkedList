9a4edf8159baf2612caa0d47fc862f52106f1c22
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2023-07-22 23:35:58 +0530

    Link List Implementation in JAVA

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+       SingleLinkedList sl = new SingleLinkedList();
+        System.out.println("Link List shown as below");
+       sl.insertFirstInList(5);
+       sl.insertFirstInList(10);
+       sl.insertFirstInList(14);
+       sl.insertLastInList(20);
+       sl.printLinkList();
+    }
+}
---
+++ src/SingleLinkedList.java
@@ -0,0 +1,52 @@
+public class SingleLinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public SingleLinkedList() {
+        this.size = 0;
+    }
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public void insertFirstInList(int data){
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+        if(tail == null){
+            tail = head;
+        }
+        size++;
+    }
+    public void printLinkList(){
+        Node node = head;
+        while (node!=null){
+            System.out.print(node.data+ " -> ");
+            node = node.next;
+        }
+        System.out.print("END");
+    }
+
+    public void insertLastInList(int data){
+        Node node = new Node(data);
+        if(tail == null){
+            insertFirstInList(data);
+        }
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+
+}
